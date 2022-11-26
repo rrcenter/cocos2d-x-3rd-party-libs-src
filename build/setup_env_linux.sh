eb521eeed39896ac2d2dc0b2f8b981ced8f068f2
local <local>
2022-11-26 22:59:56 +0800

    fix:ci: wip

--- build/setup_env_linux.sh
+++ build/setup_env_linux.sh
@@ -18,7 +18,11 @@ export ANDROID_NDK_HOME=$ANDROID_NDK'
 export ANDROID_NDK_ROOT=$ANDROID_NDK'
 export PATH=$ANDROID_NDK/toolchains/llvm/prebuilt/${NDK_PLAT}-x86_64/bin:$ANDROID_NDK/toolchains/arm-linux-androideabi-4.9/prebuilt/${NDK_PLAT}-x86_64/bin:$PATH'
 
+env
+
 echo export ANDROID_NDK=$ANDROID_NDK >> ~/.bashrc
-echo export ANDROID_NDK_HOME=$ANDROID_NDK_HOME >> ~/.bashrc
-echo export ANDROID_NDK_ROOT=$ANDROID_NDK_ROOT >> ~/.bashrc
+echo export ANDROID_NDK_HOME=$ANDROID_NDK >> ~/.bashrc
+echo export ANDROID_NDK_ROOT=$ANDROID_NDK >> ~/.bashrc
 echo export PATH=$PATH >> ~/.bashrc
+
+cat ~/.bashrc
