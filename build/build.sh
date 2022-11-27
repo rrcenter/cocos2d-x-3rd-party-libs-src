276f523fe2a0790cf27a7302e05461648039ee6e
local <local>
2022-11-27 18:52:15 +0800

    fix:ci: wip

--- build/build.sh
+++ build/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # exit this script if any commmand fails
 set -e
-set -x
+# set -x
 
 source `pwd`/main.ini
 #
--- build/entry.sh
+++ build/entry.sh
@@ -1,5 +1,7 @@
+#!/bin/bash
+
 set -e
-set -x
+# set -x
 
 
 NDK_PLAT=linux
