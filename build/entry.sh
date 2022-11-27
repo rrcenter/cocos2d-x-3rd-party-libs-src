6dcf5bfe88a8c5d5572478c4a7bf8579f8e9fa0f
local <local>
2022-11-27 18:09:54 +0800

    fix:ci: wip

--- build/entry.sh
+++ build/entry.sh
@@ -13,9 +13,9 @@ mkdir -p buildsrc
 wget -q -O ndk.zip https://dl.google.com/android/repository/android-ndk-r16b-${NDK_PLAT}-x86_64.zip
 unzip -q ndk.zip -d buildsrc/
 
-export ANDROID_NDK=`pwd`/buildsrc/android-ndk-r16b'
-export ANDROID_NDK_HOME=$ANDROID_NDK'
-export ANDROID_NDK_ROOT=$ANDROID_NDK'
-export PATH=$ANDROID_NDK/toolchains/llvm/prebuilt/${NDK_PLAT}-x86_64/bin:$ANDROID_NDK/toolchains/arm-linux-androideabi-4.9/prebuilt/${NDK_PLAT}-x86_64/bin:$PATH'
+export ANDROID_NDK=`pwd`/buildsrc/android-ndk-r16b
+export ANDROID_NDK_HOME=$ANDROID_NDK
+export ANDROID_NDK_ROOT=$ANDROID_NDK
+export PATH=$ANDROID_NDK/toolchains/llvm/prebuilt/${NDK_PLAT}-x86_64/bin:$ANDROID_NDK/toolchains/arm-linux-androideabi-4.9/prebuilt/${NDK_PLAT}-x86_64/bin:$PATH
 
 source build/build.sh -p=android --libs=luajit --mode=release
